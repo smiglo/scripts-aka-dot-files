0fa544c7924f5f1dba19d3214652cabc3e7166f7
Tomasz <tomek.blaszczak@gmail.com>
2026-09-18 18:54:46 +0000

    next

--- bin/oth/no-suspend.sh
+++ bin/oth/no-suspend.sh
@@ -3,48 +3,61 @@
 if [[ $1 == "@@" ]]; then # @@:new # {{{
   case $2 in
   -m | --msg) echo "MSG";;
-  -t | --timeout) echo "10m 30m 1h 2h";;
+  -w | --wait) echo "10m 30m 1h 2h";;
+  --pane) echo "PANE";;
   --pid) echo "PID";;
   --cmd) echo "CMD";;
   *) compl-get-args <$0;;
   esac; exit 0
 fi # }}}
 
+import-module time-tools
+
 msg=
-timeout=
+delay=
 cmd=
 pane=
 pid=
-while [[ -n $1 ]]; do
+
+while [[ -n $1 ]]; do # {{{
   case $1 in
   -m | --msg) msg="$2"; shift;;
-  -t | --timeout) timeout="$2"; shift;;
+  -w | --wait) delay="$2"; shift;;
   --pane) # {{{
     pane="$2"; shift
     [[ -z $msg ]] && msg="suspend blocked by pid: $2 in pane $pane"
-    [[ -z $timeout ]] && timeout="3h";; # }}}
+    [[ -z $delay ]] && delay="3h";; # }}}
   --pid) # {{{
     pid="$2"; shift
     [[ -z $msg ]] && msg="suspend blocked by pid: $2"
-    [[ -z $timeout ]] && timeout="3h";; # }}}
+    [[ -z $delay ]] && delay="3h";; # }}}
   --cmd) # {{{
     cmd="$2"
     [[ -z $msg ]] && msg="suspend blocked by cmd: $2"
-    [[ -z $timeout ]] && timeout="3h"
+    [[ -z $delay ]] && delay="3h"
     shift;; # }}}
-  *) timeout="$1"; shift $#;;
+  *) # {{{
+    if [[ $1 =~ .*\.[0-9]+$ ]]; then
+      pane=$1
+      [[ -z $delay ]] && delay="3h"
+    elif [[ $1 =~ ^[0-9]+$ ]] || time2s --is-hms $1; then
+      delay="$1"; shift $#
+    else
+      pid=$($ENV_SCRIPTS/tmux/tm.sh --pids -a | awk '$3 ~ /^'"$1"'/ {print $1}' | head -n1)
+      [[ -n $pid ]] || die "cannot find running command '^$1'"
+      [[ -z $msg ]] && msg="suspend blocked by pid: $2/command: $1"
+      [[ -z $delay ]] && delay="3h"
+    fi # }}}
   esac; shift
-done
-
-import-module time-tools
+done # }}}
 
 [[ -n $msg ]] || msg="suspend blocked"
-[[ -n $timeout ]] || timeout="1h"
-timeout=$(time2s $timeout -o s)
+[[ -n $delay ]] || delay="1h"
+delay=$(time2s $delay -o s)
 
 params=(
   --no-err
-  --background-cmd "systemd-inhibit --why='$msg' --who='$USER' --what=sleep sleep $((timeout-3))"
+  --background-cmd "systemd-inhibit --why='$msg' --who='$USER' --what=sleep sleep $((delay-3))"
 )
 [[ -n $pane ]] && params+=(
   --pane "$pane"
@@ -56,4 +69,4 @@ params=(
   --check-cmd "$cmd"
 )
 
-exec progress-dot-wrapper "${params[@]}" $timeout
+exec progress-dot-wrapper "${params[@]}" $delay
